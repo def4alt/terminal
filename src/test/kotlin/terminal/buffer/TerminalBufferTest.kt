538c31f1e39cf543c2f358aa67077d9a14df1276
Andrii Olkhovych <andrii.olkhovych@tum.de>
2026-03-12 09:56:18 +0100

    build: initialize Kotlin Gradle project

---
+++ src/test/kotlin/terminal/buffer/TerminalBufferTest.kt
@@ -0,0 +1,11 @@
+package terminal.buffer
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TerminalBufferTest {
+    @Test
+    fun placeholder() {
+        assertEquals(1, 1)
+    }
+}
